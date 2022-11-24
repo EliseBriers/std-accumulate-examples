0f9ad2eb1ca0573032e3813871c0ed2d68aefc62
EliseBriers <elise.briers@outlook.com>
2022-11-24 19:51:38 +0100

    Fix incorrect gitignore

--- build/CMakeFiles/std_accumulate_examples.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for std_accumulate_examples.
-# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "catch2/catch_amalgamated.hpp"
+// #include <iostream>
+
+// int main(int argc, char *argv[])
+// {
+//    std::cout << "Hello CMake!" << std::endl;
+//    return 0;
+// }
+
+TEST_CASE("Test") { REQUIRE(true); }
